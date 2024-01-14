81c78faa6a317dbffbc584bf0f497a2046025f17
amnaalik <156576497+amnaalik@users.noreply.github.com>
2024-01-14 15:08:06 +0500

    Add files via upload

---
+++ mybook.py
@@ -0,0 +1,21 @@
+('''
+addition
+subtraction
+multiplication
+division
+''')
+num1=int(input("enter the value num 1"))
+num2=int(input("enter the value num2"))
+if opr ==(input("enter the opr.....")):
+        if opr == "+":
+        print  opr==(num1+num2)
+        elif opr== "-":
+            print(num 1 - num2)
+            elif opr =="*":
+                print (num1*num2):
+                    elif opr == "/":
+                        print (num1/num2)
+                        else:
+                            print ("invlaid opr")
+                            
+            
